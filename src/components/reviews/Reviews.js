e0a80ed9ff0a0fb95eafaa34a8782c1928cd673d
Carla Holcomb <carla.programmer@gmail.com>
2021-01-31 21:30:03 +0000

    Automatically backed up by Learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -7,7 +7,7 @@ class Reviews extends Component {
 
     const { reviews, restaurantId, deleteReview } = this.props;
     const associatedReviews = reviews.filter(review => review.restaurantId === restaurantId);
-    
+
     const reviewList = associatedReviews.map((review, index) => {
       return <Review key={index} review={review} deleteReview={deleteReview} />
     })
